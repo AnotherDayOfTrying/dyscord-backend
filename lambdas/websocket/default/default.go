31a4171791b3659ebc660324d488e03c5a337936
justin javier <javierjustin7@gmail.com>
2025-03-25 16:54:45 -0600

    fix: lambda code responses

--- lambdas/websocket/default/default.go
+++ lambdas/websocket/default/default.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	responseBody, err := json.Marshal(map[string]string{
+		"message": "Default!",
+	})
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal Sever Error"}, nil
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body: "Disconnected!",
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(responseBody),
 	}, nil
 }
 
