d5b5a09ab52f33e2a8efe9b68dd416bb63c7f89c
justin javier <javierjustin7@gmail.com>
2025-04-15 17:23:30 -0600

    test: testing update function for dynamodb streams

--- lambdas/websocket/update/update.go
+++ lambdas/websocket/update/update.go
@@ -3,17 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
-	"reflect"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 
 	"dyscord-backend/lambdas/services"
 )
@@ -32,44 +27,24 @@ func init() {
 	}
 }
 
-func handler(ctx context.Context, request events.DynamoDBStreamRecord) error {
-	var call services.Call
+func handler(ctx context.Context, request json.RawMessage) error {
+	// var call services.Call
 	log.Println(request)
-	log.Println(ctx)
-	log.Println(request.NewImage)
-	val := reflect.ValueOf(request)
-	typ := reflect.TypeOf(request)
-
-	for i := 0; i < val.NumField(); i++ {
-		field := typ.Field(i)
-		value := val.Field(i)
-
-		fmt.Printf("%s: %v\n", field.Name, value.Interface())
-	}
-
-	val = reflect.ValueOf(request.NewImage)
-	typ = reflect.TypeOf(request.NewImage)
-
-	for i := 0; i < val.NumField(); i++ {
-		field := typ.Field(i)
-		value := val.Field(i)
-
-		fmt.Printf("%s: %v\n", field.Name, value.Interface())
-	}
-	if request, ok := any(request.NewImage).(map[string]types.AttributeValue); ok {
-		attributevalue.UnmarshalMap(request, &call)
-		value, err := json.Marshal(call.ConnectionSdps)
-		if err != nil {
-			return err
-		}
-		connectionIds := make([]string, len(call.ConnectionSdps))
-		for index, sdp := range call.ConnectionSdps {
-			connectionIds[index] = sdp.ConnectionId
-		}
-		api.PostToConnections(ctx, connectionIds, value)
-	} else {
-		return errors.New("could not unmarshall request")
-	}
+	log.Println(string(request))
+	// if request, ok := any(request.NewImage).(map[string]types.AttributeValue); ok {
+	// 	attributevalue.UnmarshalMap(request, &call)
+	// 	value, err := json.Marshal(call.ConnectionSdps)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	connectionIds := make([]string, len(call.ConnectionSdps))
+	// 	for index, sdp := range call.ConnectionSdps {
+	// 		connectionIds[index] = sdp.ConnectionId
+	// 	}
+	// 	api.PostToConnections(ctx, connectionIds, value)
+	// } else {
+	// 	return errors.New("could not unmarshall request")
+	// }
 	return nil
 }
 
