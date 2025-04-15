d36c145d4f54f74caeef38a58529dc7a9a611c2e
justin javier <javierjustin7@gmail.com>
2025-04-15 17:48:41 -0600

    feat: dynamodb stream updates

--- lambdas/websocket/update/update.go
+++ lambdas/websocket/update/update.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 
 	"dyscord-backend/lambdas/services"
 )
@@ -27,24 +30,31 @@ func init() {
 	}
 }
 
-func handler(ctx context.Context, request json.RawMessage) error {
-	// var call services.Call
-	log.Println(request)
-	log.Println(string(request))
-	// if request, ok := any(request.NewImage).(map[string]types.AttributeValue); ok {
-	// 	attributevalue.UnmarshalMap(request, &call)
-	// 	value, err := json.Marshal(call.ConnectionSdps)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	connectionIds := make([]string, len(call.ConnectionSdps))
-	// 	for index, sdp := range call.ConnectionSdps {
-	// 		connectionIds[index] = sdp.ConnectionId
-	// 	}
-	// 	api.PostToConnections(ctx, connectionIds, value)
-	// } else {
-	// 	return errors.New("could not unmarshall request")
-	// }
+func handler(ctx context.Context, request events.DynamoDBEvent) error {
+	var call services.Call
+	for _, record := range request.Records {
+		if record.EventName == "MODIFY" && record.Change.StreamViewType == "NEW_IMAGE" {
+			if image, ok := any(record.Change.NewImage).(map[string]types.AttributeValue); ok {
+				err := attributevalue.UnmarshalMap(image, &call)
+
+				if err != nil {
+					log.Println("Could not unmarshal map")
+				}
+				connectionIds := make([]string, len(call.ConnectionSdps))
+				for index, sdp := range call.ConnectionSdps {
+					connectionIds[index] = sdp.ConnectionId
+				}
+
+				value, err := json.Marshal(call.ConnectionSdps)
+
+				if err != nil {
+					log.Println("Could not marshal connection sdps")
+				}
+
+				api.PostToConnections(ctx, connectionIds, value)
+			}
+		}
+	}
 	return nil
 }
 
